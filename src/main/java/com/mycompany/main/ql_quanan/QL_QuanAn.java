70739f61b8838f9c6cf86719a02bc14c6e7e7660
ThanhPhatFPT <156346544+ThanhPhatFPT@users.noreply.github.com>
2024-06-24 20:53:34 +0700

    how to fix?

---
+++ src/main/java/com/mycompany/main/ql_quanan/QL_QuanAn.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.main.ql_quanan;
+
+/**
+ *
+ * @author votha
+ */
+public class QL_QuanAn {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
